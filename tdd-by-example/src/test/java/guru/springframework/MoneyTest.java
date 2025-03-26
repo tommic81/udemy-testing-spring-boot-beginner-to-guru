63cab66be08679004cfb9b351bbf7107294c2f0e
tommic81 <tom.michnik@gmail.com>
2025-03-26 08:11:19 +0100

    Implemented 'Change', updated docs

--- tdd-by-example/src/test/java/guru/springframework/MoneyTest.java
+++ tdd-by-example/src/test/java/guru/springframework/MoneyTest.java
@@ -67,4 +67,19 @@ public class MoneyTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    void testReduceMoneyDifferentCurrency(){
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.frank(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    void testIndentityRate(){
+        assertEquals(1, new Bank().rate("USD", "USD"));
+        assertEquals(1, new Bank().rate("CHF", "CHF"));
+    }
+
 }
